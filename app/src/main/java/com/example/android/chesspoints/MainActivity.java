9dd4dbc9572226a122860dc8fba20cd048864103
Alex Gidei <alex.gidei@gmail.com>
2018-02-18 18:43:50 +0000

    Third commit - improved UX across devices

--- app/src/main/java/com/example/android/chesspoints/MainActivity.java
+++ app/src/main/java/com/example/android/chesspoints/MainActivity.java
@@ -5,36 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.android.chesspoints.R;
-
 public class MainActivity extends AppCompatActivity {
 
-    int scoreWhite = 0;
-    int scoreBlack = 0;
     static final String savedWhite = "scoreWhite";
     static final String savedBlack = "scoreBlack";
+    int scoreWhite = 0;
+    int scoreBlack = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState){
+    public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt(savedWhite, scoreWhite);
         savedInstanceState.putInt(savedBlack, scoreBlack);
-
         super.onSaveInstanceState(savedInstanceState);
     }
 
-    public void onRestoreInstanceState(Bundle savedInstanceState){
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
         scoreWhite = savedInstanceState.getInt(savedWhite);
         scoreBlack = savedInstanceState.getInt(savedBlack);
-
         displayForWhite(scoreWhite);
         displayForBlack(scoreBlack);
     }
@@ -43,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for White.
      */
     public void displayForWhite(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.white_score);
+        TextView scoreView = findViewById(R.id.white_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -51,10 +45,13 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Black.
      */
     public void displayForBlack(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.black_score);
+        TextView scoreView = findViewById(R.id.black_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * The methods below add points for white/black.
+     */
     public void addOneForWhite(View v) {
         scoreWhite += 1;
         displayForWhite(scoreWhite);
@@ -73,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
     public void addThreeForBlack(View v) {
         scoreBlack += 3;
         displayForBlack(scoreBlack);
-
     }
+
     public void addFiveForWhite(View v) {
         scoreWhite += 5;
         displayForWhite(scoreWhite);
@@ -84,7 +81,9 @@ public class MainActivity extends AppCompatActivity {
         scoreBlack += 5;
         displayForBlack(scoreBlack);
 
-    }public void addNineForWhite(View v) {
+    }
+
+    public void addNineForWhite(View v) {
         scoreWhite += 9;
         displayForWhite(scoreWhite);
     }
@@ -92,9 +91,11 @@ public class MainActivity extends AppCompatActivity {
     public void addNineForBlack(View v) {
         scoreBlack += 9;
         displayForBlack(scoreBlack);
-
     }
 
+    /**
+     * Resets the score for both sides.
+     */
     public void resetScore(View v) {
         scoreWhite = 0;
         scoreBlack = 0;
